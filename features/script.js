51fa2dcd7e17bf61b300d4f262d3cbcf62399be7
Himanshu310805 <himanshufirake@gmail.com>
2024-09-13 11:46:08 +0530

    Add files via upload

---
+++ features/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your submission! We will review your details and get back to you soon.');
+});
